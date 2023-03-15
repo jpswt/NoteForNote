640ce76bcf870150489172eac90a3950053985a3
jpswt <joel_swt@yahoo.com>
2023-03-15 14:28:18 -0500

    update register

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,6 +11,7 @@ router.post('/register', async (req, res) => {
 			username: req.body.username,
 			email: req.body.email,
 			password: hashedPW,
+			profilePic: '../assets/default.jpeg',
 		});
 
 		const user = await newUser.save();
