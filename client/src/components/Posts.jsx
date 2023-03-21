d06dffaf46d7b0dd2ca3da25cd062c2deda34cbc
jpswt <joel_swt@yahoo.com>
2023-03-21 14:23:19 -0500

    fix home loading

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import SinglePost from './SinglePost';
 
 const Posts = ({ posts, searchResult }) => {
+	const [isLoaded, setIsLoaded] = useState(false);
 	const results = searchResult.map((post, i) => (
 		<SinglePost post={post} key={i} />
 	));
+
+	useEffect(() => {
+		const loaded = () => {
+			{
+				results?.length > 0 && setIsLoaded(true);
+			}
+		};
+		loaded();
+	}, [results]);
 
 	const content = results?.length ? (
 		results
@@ -16,8 +26,8 @@ const Posts = ({ posts, searchResult }) => {
 	);
 
 	return (
-		<div className=" flex-9 flex flex-col items-center w-[80%] mt-4">
-			{content}
+		<div className=" flex-9 flex flex-col items-center mt-4">
+			{isLoaded ? content : <></>}
 		</div>
 	);
 };
