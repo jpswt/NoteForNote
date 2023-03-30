6b2d65167cd791f2e9fb84f365a06ad04e63af90
jpswt <joel_swt@yahoo.com>
2023-03-29 23:06:48 -0500

    fix profile

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -40,8 +40,6 @@ const Settings = () => {
 			setTimeout(() => {
 				setSuccessMsg(false);
 			}, 1500);
-			navigate('/profile');
-			window.location = window.location;
 			// console.log(updateUser);
 		} catch (err) {
 			dispatch({ type: 'UPDATE_FAIL' });
