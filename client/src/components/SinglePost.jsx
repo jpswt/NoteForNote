80a7c4c4861adac7cedbedb7de70c40c04243d9a
jpswt <joel_swt@yahoo.com>
2023-04-01 00:14:05 -0500

    fix default img

--- client/src/components/SinglePost.jsx
+++ client/src/components/SinglePost.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import DOMpurify from 'dompurify';
 import defaultPic from '../assets/default.jpeg';
+import defaultPostPic from '../assets/guitar_default.jpeg';
 
 const SinglePost = ({ post }) => {
 	const publicFolder = `${import.meta.env.VITE_NFN_URI}/assets/`;
@@ -69,7 +70,7 @@ const SinglePost = ({ post }) => {
 					<Link to={`/posts/${post._id}`}>
 						<img
 							className="w-[100px] h-[100px] object-cover rounded-sm p-0 mx-6 hover:opacity-90 sm:w-[60px] sm:h-[60px]"
-							src={publicFolder + post.photo}
+							src={publicFolder + post.photo || defaultPostPic}
 							alt=""
 						/>
 					</Link>
