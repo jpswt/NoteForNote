328dc3bda2f3736bcd6b4a6866699bacb3c13a25
jpswt <joel_swt@yahoo.com>
2023-04-02 13:25:37 -0500

    switching local disk to firebase storage

--- backend/index.js
+++ backend/index.js
@@ -12,33 +12,51 @@ const path = require('path');
 const PORT = process.env.PORT || 6000;
 
 const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(express.json());
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
+
+var admin = require('firebase-admin');
+
+var serviceAccount = require('./serviceAcctKey.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	storageBucket: process.env.BUCKET_URL,
+});
+
+app.locals.bucket = admin.storage().bucket();
 
 mongoose
 	.connect(process.env.MONGO_URL)
 	.then(console.log('Connected to MongoDB'))
 	.catch((err) => console.error(err));
 
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'assets');
-	},
-	filename: (req, file, cb) => {
-		cb(null, req.body.name);
-	},
-});
+// const storage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, 'assets');
+// 	},
+// 	filename: (req, file, cb) => {
+// 		cb(null, req.body.name);
+// 	},
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
 // app.use('/', (req, res) => {
 // 	console.log('Welcome to my server');
 // 	res.json('Welcome to my MongoDB Blog Server');
 // });
-app.post('/upload', upload.single('file'), (req, res) => {
-	res.status(200).json('file has been uploaded');
+app.post('/upload', upload.single('file'), async (req, res) => {
+	const fileName = req.body.name;
+	await app.locals.bucket
+		.file(fileName)
+		.createWriteStream()
+		.end(req.file.buffer);
+	res.send('file has been uploaded');
+	// res.status(200).json('file has been uploaded');
 });
 
 app.use('/auth', authRoute);
